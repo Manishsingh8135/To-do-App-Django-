7629121f4233c87bbccdf5e47aab91b2ed76c71a
Manishsingh8135 <manishsingh8135@gmail.com>
2023-04-22 21:50:45 +0530

    Added restriction

--- base/views.py
+++ base/views.py
@@ -4,7 +4,11 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
+
 
 
 
@@ -24,25 +28,25 @@ class CustomLoginView(LoginView):
 
 
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin,ListView):
     model=Task
     context_object_name='tasks'
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin,DetailView):
     model=Task
     context_object_name='task'
     template_name='base/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     model=Task
     fields='__all__'
     success_url=reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model=Task
     fields='__all__'
     success_url=reverse_lazy('tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     model=Task
     context_object_name='task'
     success_url=reverse_lazy('tasks')
